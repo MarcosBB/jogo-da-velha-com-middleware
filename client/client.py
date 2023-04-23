906c8a8f5f1d8ae88bfa0a0c049a32ad6cc98aa7
MarcosBB <marcosberaldobarros@hotmail.com>
2023-04-23 16:57:56 -0300

    ✨feat: playing game

--- client/client.py
+++ client/client.py
@@ -11,6 +11,44 @@ print(f"Procurando por um oponente...")
 
 game_id = game.match_making(player.get_id())
 game.load_game(game_id)
-print(game.get_game_data())
+player.load_player()
+print()
+
+while True:
+    game.load_game(game_id)
+    game_data = game.get_game_data()
+    if game_data["player2"]:
+        break
+    else:
+        print("Aguardando um oponente...")
+        time.sleep(1)
+        os.system("clear")
 
 
+while True:
+    game.check_win()
+    game.load_game(game_id)
+    game_data = game.get_game_data()
+
+    print(game_data["board"][0] + "|" + game_data["board"][1] + "|" + game_data["board"][2])
+    print(game_data["board"][3] + "|" + game_data["board"][4] + "|" + game_data["board"][5])
+    print(game_data["board"][6] + "|" + game_data["board"][7] + "|" + game_data["board"][8])
+
+    if game_data["winner"]:
+        break
+
+    if game_data["current_player"] == player.get_id():
+        print("É a sua vez!")
+        position = int(input("Insira a posição que deseja jogar:"))
+        game.do_move(position, player.get_id())
+    else:
+        print("Aguarde a vez do seu oponente...")
+        time.sleep(1)
+        os.system("clear")
+
+
+
+if game_data["winner"] == player.get_id():
+    print("You won!")
+else:
+    print("You lost!")
--- server/jogo_da_velha/JogoDaVelha.py
+++ server/jogo_da_velha/JogoDaVelha.py
@@ -14,7 +14,8 @@ class Player:
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS player (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL
+                name TEXT NOT NULL,
+                symbol TEXT
             )
         ''')
         self.database.commit()
@@ -34,6 +35,14 @@ class Player:
 
         self.database.commit()
 
+    def load_player(self, player_id=None):
+        id = player_id if player_id else self.id
+        self.cursor.execute("SELECT * FROM player WHERE id=?", (id,))
+        player_data = self.cursor.fetchone()
+        self.id = player_data[0]
+        self.name = player_data[1]
+        self.symbol = player_data[2]
+
 
     def get_name(self):
         return self.name
@@ -41,6 +50,8 @@ class Player:
     def get_id(self):
         return self.id
 
+    def get_symbol(self):
+        return self.symbol
 @Pyro4.expose
 class JogoDaVelha:
     def __init__(self):
@@ -68,6 +79,7 @@ class JogoDaVelha:
         game_data = self.cursor.fetchone()
         self.player1 = game_data[1]
         self.player2 = game_data[2]
+        self.winner = game_data[3]
         self.current_player = game_data[4]
         self.board = json.loads(game_data[5])
 
@@ -78,6 +90,7 @@ class JogoDaVelha:
             'player2': self.player2,
             'board': self.board,
             'current_player': self.current_player,
+            'winner': self.winner
         }   
 
     def create_game(self, player1_id):
@@ -92,14 +105,26 @@ class JogoDaVelha:
         return game_data[0]
     
     def join_game(self, player2_id, game_id):
-        self.cursor.execute("UPDATE game SET player2 = ? WHERE id = ?", (player2_id, game_id))
-        self.database.commit()
-        
-        self.cursor.execute("SELECT id FROM game WHERE id=?", (game_id,))
+        self.cursor.execute("SELECT id, player1 FROM game WHERE id=?", (game_id,))
         game_data = self.cursor.fetchone()
-        return game_data[0]
+
+        current_player = random.choice([game_data[1], player2_id])
+        player1_symbol = random.choice(["X", "O"]) 
+        player2_symbol = "X" if player1_symbol == "O" else "O"
+
+        self.cursor.execute("UPDATE player SET symbol = ? WHERE id = ?", (player1_symbol, game_data[1]))
+        self.cursor.execute("UPDATE player SET symbol = ? WHERE id = ?", (player2_symbol, player2_id))
+        self.cursor.execute("UPDATE game SET player2 = ?, current_player = ? WHERE id = ?", (player2_id, current_player, game_id))
+        self.database.commit()
+
+        return game_id
     
     def match_making(self, player_id):
+        """TODO: Melhorar a lógica de matchmaking
+        - retomar partidas inacabadas
+        - sair da partida (excluir do banco de dados)
+        
+        """
         self.cursor.execute("SELECT * FROM game WHERE player2 IS NULL AND player1 != ?", (player_id,))
         
         game_data = self.cursor.fetchone()
@@ -108,3 +133,44 @@ class JogoDaVelha:
         else:
             return self.create_game(player_id)
 
+    def check_win(self):
+        def set_winner(symbol):
+            if self.player1.symbol == symbol:
+                winner = self.player1.id
+            else:
+                winner = self.player2.id
+            self.cursor.execute("UPDATE game SET winner = ? WHERE id = ?", (winner, self.id))
+            self.database.commit()
+            return winner
+
+        for i in range(0, 9, 3):
+            if self.board[i] == self.board[i+1] == self.board[i+2] != '-':
+                set_winner(self.board[i])
+        for i in range(3):
+            if self.board[i] == self.board[i+3] == self.board[i+6] != '-':
+                set_winner(self.board[i])
+        if self.board[0] == self.board[4] == self.board[8] != '-':
+            set_winner(self.board[0])
+        if self.board[2] == self.board[4] == self.board[6] != '-':
+            set_winner(self.board[2])
+    
+    def _swich_player(self):
+        if self.current_player == self.player1:
+            self.current_player = self.player2
+        else:
+            self.current_player = self.player1
+        self.cursor.execute("UPDATE game SET current_player = ? WHERE id = ?", (self.current_player, self.id))
+        self.database.commit()
+
+    def do_move(self, position, player_id):
+        player = Player()
+        player.load_player(player_id)
+        if self.board[position] == '-' and self.current_player == player_id:
+            
+            self.board[position] = player.symbol
+            self.cursor.execute("UPDATE game SET board = ? WHERE id = ?", (json.dumps(self.board), self.id))
+            self.database.commit()
+            self._swich_player()
+            return True
+        else:
+            return False
